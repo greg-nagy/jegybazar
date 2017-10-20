38bc0d9d091fa5c62a1211a13500382cb6835014
puffy <nagy.gergo@3pixel.hu>
2017-10-20 20:12:17 +0200

    component darabolas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CollapseModule } from 'ngx-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { EventcardComponent } from './eventcard/eventcard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    EventcardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/eventcard/eventcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventcard',
+  templateUrl: './eventcard.component.html',
+  styleUrls: ['./eventcard.component.css']
+})
+export class EventcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
