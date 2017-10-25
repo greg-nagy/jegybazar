d420cae40f7fe0a4aa1d3744fe728728383928dc
puffy <nagy.gergo@3pixel.hu>
2017-10-25 19:33:22 +0200

    event resz build atemeles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
 import { EventComponent } from './event/event.component';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TicketComponent } from './ticket/ticket.component';
-import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'event', component: EventComponent},
+  {
+    path: 'event',
+    component: EventComponent,
+    children: [
+      {path: 'list', component: EventListComponent},
+      {path: 'new', component: EventDetailComponent},
+      {path: ':id/edit', component: EventDetailComponent}
+    ]
+  },
   {path: 'ticket', component: TicketComponent},
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
@@ -28,6 +38,8 @@ export class AppRoutingModule {
   static routableComponents = [
     HomeComponent,
     EventComponent,
+    EventListComponent,
+    EventDetailComponent,
     TicketComponent,
     AboutComponent,
     LoginComponent,
