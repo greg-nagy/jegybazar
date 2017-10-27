c4a575e7b8dbea89970854720b9046b12ca2dc81
puffy <nagy.gergo@3pixel.hu>
2017-10-27 18:37:29 +0200

    3-as tagolas bevezetese es nyers service adatok atstrukturalsa a nevezet elvarasainak megfeleloen

--- src/app/event/event-list/event-list.component.ts
+++ src/app/event/event-list/event-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from '../../shared/event.service';
 import { EventModel } from '../../shared/event-model';
+import { EventService } from '../../shared/event.service';
 
 @Component({
   selector: 'app-event-list',
@@ -9,14 +9,23 @@ import { EventModel } from '../../shared/event-model';
 })
 export class EventListComponent implements OnInit {
 // ez jo pelda lehet smart es dumb componentre
-  public events: EventModel[];
+  public eventsGrouppedBy3: EventModel[];
 
   constructor(private _eventService: EventService) {
-    this.events = this._eventService.getAllEvents();
-    console.log(this.events);
   }
 
   ngOnInit() {
+    // ind!! [0,1,2,3,4,5,6,7,8] -- reduce --> [[0,1,2],[3,4,5],[6,7,8]]
+    this.eventsGrouppedBy3 = this._eventService.getAllEvents()
+      .reduce((acc, curr: EventModel, ind: number) => {
+        if (ind % 3 === 0) {
+          acc.push([]);
+        }
+        acc[acc.length - 1].push(curr);
+        return acc;
+      }, []);
+    console.log(this.eventsGrouppedBy3);
   }
 
 }
+
