a69ceed940cdee09e19adbf2fc88ca7be0a501b8
eggp <eggprojec@gmail.com>
2017-11-06 18:38:36 +0100

    (modify): bid component add event card

--- src/app/ticket/bid/bid.component.ts
+++ src/app/ticket/bid/bid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TicketService } from '../../shared/ticket.service';
+import { TicketModel } from '../../shared/ticket-model';
 
 @Component({
   selector: 'app-bid',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bid.component.css']
 })
 export class BidComponent implements OnInit {
+  ticket: TicketModel;
 
-  constructor() { }
+  constructor(private ticketService: TicketService) { }
 
   ngOnInit() {
+    const id = '-Ky0HolLJBH3Q5uVHWZf';
+    this.ticketService.getOne(id).subscribe(
+      ticket => this.ticket = ticket
+    );
   }
 
 }
