2ac7ceabf8f0bb2eec61e36c6d390f6057ec7bdd
eggp <eggprojec@gmail.com>
2017-11-10 20:22:20 +0100

    (modify): bid component ticket replace to stream base

--- src/app/ticket/bid/bid.component.ts
+++ src/app/ticket/bid/bid.component.ts
@@ -4,6 +4,7 @@ import { TicketModel } from '../../shared/ticket-model';
 import { UserService } from '../../shared/user.service';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/share';
 
 @Component({
   selector: 'app-bid',
@@ -11,7 +12,7 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./bid.component.css']
 })
 export class BidComponent implements OnInit {
-  ticket: TicketModel;
+  ticket$: Observable<TicketModel>;
   isLoggedIn$: Observable<boolean>;
   progressRefreshTicket = false;
 
@@ -32,23 +33,18 @@ export class BidComponent implements OnInit {
     );
   }
 
-  onRefreshTicket() {
-    this.refreshTicket(this.ticket.id);
-  }
-
   private refreshTicket(id: string) {
     this.progressRefreshTicket = true;
     const handle404 = () => {
       this.router.navigate(['404']);
     };
 
-    this.ticketService.getOne(id).subscribe(
+    this.ticket$ = this.ticketService.getOne(id).share();
+    this.ticket$.subscribe(
       ticket => {
         this.progressRefreshTicket = false;
         if (ticket === null) {
           handle404();
-        } else {
-          this.ticket = ticket;
         }
       },
       err => {
