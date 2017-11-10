8495496bc706b77cce1c12eb4067e7e6e5faaf9b
eggp <eggprojec@gmail.com>
2017-11-10 18:13:15 +0100

    (modify): bid form add loading spinner

--- src/app/core/loading-spinner/loading-spinner.component.ts
+++ src/app/core/loading-spinner/loading-spinner.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: ['./loading-spinner.component.css']
 })
-export class LoadingSpinnerComponent {}
+export class LoadingSpinnerComponent {
+  @Input() loaderWidth = 120;
+  @Input() loaderHeight = 120;
+}
