6d18abbcab7142c9ec6d2afff9d9c36161c6916e
eggp <eggprojec@gmail.com>
2017-11-13 20:52:43 +0100

    (modify): ticket details card detach cdr

--- src/app/ticket/ticket-details-card/ticket-details-card.component.ts
+++ src/app/ticket/ticket-details-card/ticket-details-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { TicketModel } from '../../shared/ticket-model';
 
 @Component({
@@ -6,7 +6,37 @@ import { TicketModel } from '../../shared/ticket-model';
   templateUrl: './ticket-details-card.component.html',
   styleUrls: ['./ticket-details-card.component.css']
 })
-export class TicketDetailsCardComponent {
+export class TicketDetailsCardComponent implements AfterViewInit, OnChanges {
   @Input() ticket: TicketModel;
   @Input() loading = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes['loading'] != null
+      && changes['loading'].currentValue !== changes['loading'].previousValue) {
+      this.cdr.detectChanges();
+    } else if (changes['ticket'] != null
+      && changes['ticket'].isFirstChange() === false) {
+      const prev: TicketModel = changes['ticket'].previousValue;
+      const current: TicketModel = changes['ticket'].currentValue;
+
+      if (prev == null || current == null) {
+        this.cdr.detectChanges();
+      } else if (prev.seller.name !== current.seller.name) {
+        this.cdr.detectChanges();
+      } else if (prev.numberOfTickets !== current.numberOfTickets) {
+        this.cdr.detectChanges();
+      } else if (prev.bidEndDate !== current.bidEndDate) {
+        this.cdr.detectChanges();
+      } else if (prev.details !== current.details) {
+        this.cdr.detectChanges();
+      }
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.cdr.detach();
+  }
 }
