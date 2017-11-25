0b42945f44d64f9fd4da1a0ad143b807ea6cfd5f
eggp <eggprojec@gmail.com>
2017-11-25 11:45:48 +0100

    about detach change detector

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.cdr.detach();
+  }
 }
