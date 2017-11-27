6b73ccf2d73b72e00ddf61041612f4e2376b2842
eggp <eggprojec@gmail.com>
2017-11-27 19:53:19 +0100

    chat windows css correction chat window config add friend property chat handle chat window close output

--- src/app/chat/model/chat-window.config.ts
+++ src/app/chat/model/chat-window.config.ts
@@ -1,6 +1,9 @@
+import { ChatFriendModel } from './chat-friend.model';
+
 export interface ChatWindowConfig {
   id?: string;
   title: string;
   roomId: string;
   closeable?: boolean;
+  friend: ChatFriendModel;
 }
